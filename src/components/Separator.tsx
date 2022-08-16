12fcdfac95b4a75f6f3ff5a330c32a2c14776592
Luis Branco <luisjbrancog@gmail.com>
2022-08-16 20:06:16 +0100

    Created a component to separate components

---
+++ src/components/Separator.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { TbSquareRotated } from "react-icons/tb";
+
+const Separator = () => {
+  return (
+    <div className=" h-[200px] flex items-center justify-center bg-[#413F42]">
+      <TbSquareRotated size={30} style={{ fill: "#D2042D" }} />
+      <TbSquareRotated size={30} style={{ fill: "#D2042D" }} />
+      <TbSquareRotated size={30} style={{ fill: "#D2042D" }} />
+    </div>
+  );
+};
+
+export default Separator;
