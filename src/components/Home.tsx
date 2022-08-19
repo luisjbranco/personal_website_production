2d3ee66c2591d97d90be2040d91493051defde04
Luis Branco <luisjbrancog@gmail.com>
2022-08-19 10:22:13 +0100

    Removed Image import

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { profilePic } from "../assets";
 import { HiArrowNarrowRight } from "react-icons/hi";
-import Image from "next/image";
 
 const Home = () => {
   return (
